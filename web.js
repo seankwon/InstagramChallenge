bbbe357198c9f48d7fed14ff862122c17beb21f1
Sean Kwon <seankwon@Sean-Kwons-MacBook.local>
2014-05-31 23:19:32 -0400

    stupid vim

--- web.js
+++ web.js
@@ -4,4 +4,4 @@ var app = express();
  
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);)
+app.listen(process.env.PORT || 5000);
